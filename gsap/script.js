e14fe441464fa66016d70214cfa6ceb3218721a9
minukp <minuoct20@gmail.com>
2022-03-30 18:38:18 +0530

    gsap 3 basics

---
+++ gsap/script.js
@@ -0,0 +1,20 @@
+// gsap.from("header",{duration:1,y:"-100%" , ease: "bounce"});
+// gsap.from(".link",{duration:1, opacity:0,delay:1,stagger:0.5});
+// gsap.from(".right",{duration:2, x:"-100vw",delay:1,ease:"power2.in"});
+// gsap.from(".left",{duration:1, x:"-100vw",delay:0.5});
+// gsap.to("footer",{duration:1, y:0,ease:"elastic",delay:2.5});
+// gsap.fromTo("button",{opacity:0,scale:0,rotation:720},{duration:1,delay:3.5,opacity:1,scale:1,rotation:0});
+
+const timeline = gsap.timeline({defaults: {duration:1}});
+timeline
+.from("header",{y:"-100%" , ease: "bounce"})
+.from(".link",{ opacity:0,stagger:0.5})
+.from(".right",{ x:"-100vw",ease:"power2.in"})
+.from(".left",{ x:"-100vw"},'<.5')
+.to("footer",{ y:0,ease:"elastic"})
+.fromTo("button",{opacity:0,scale:0,rotation:720},{opacity:1,scale:1,rotation:0});
+
+document.querySelector('button').addEventListener("click",()=>{
+    timeline.timeScale(3);
+    timeline.reverse();
+})
